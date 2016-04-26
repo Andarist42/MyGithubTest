469b72d120db959065a2724214349c4d742c8e74
Andarist42 <christian.hoelzl2@gmx.at>
2016-04-26 17:33:39 +0200

    added feature1 text

--- testgit.cpp
+++ testgit.cpp
@@ -1,1 +1,10 @@
 // just here to test git, no code incoming!
+
+// some changes made to the file
+// whatever text here
+
+// and now the main part of this text
+// .... nothing!
+
+// this is feature one, (everything above was master, with 1 commit and a 2nd one thrown?)
+// blabla more text on feature1 only woot!!
