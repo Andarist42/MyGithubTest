2c6393d491cc1cbad6a4c294d49fe9e7af8d5452
Andarist42 <christian.hoelzl2@gmx.at>
2016-04-27 11:59:27 +0200

    new master

--- testgit.cpp
+++ testgit.cpp
@@ -1,16 +1,3 @@
 // just here to test git, no code incoming!
 
 
-// THIS is the REAL MASTER. !!!
-
-// some changes made to the file
-// whatever text here
-
-// and now the main part of this text
-// .... nothing!
-
-// this is feature one, (everything above was master, with 1 commit and a 2nd one thrown?)
-// blabla more text on feature1 only woot!!
-
-// new commit text here in feature 1
-
